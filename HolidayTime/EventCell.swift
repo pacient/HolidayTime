7a17d63958964de7b580650c02839442b3e80e0c
Vasil Nunev <vnunev2@gmail.com>
2017-05-27 08:58:27 +0100

    create mockup for the event menu

---
+++ HolidayTime/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  HolidayTime
+//
+//  Created by Vasil Nunev on 27/05/2017.
+//  Copyright © 2017 nunev. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HolidayTime/EventTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventTableViewController.swift
+//  HolidayTime
+//
+//  Created by Vasil Nunev on 27/05/2017.
+//  Copyright © 2017 nunev. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventCell") as! EventCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
