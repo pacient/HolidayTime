1a84b8b55194ca0cb2c1f28162045923e5d9b748
Vasil Nunev <vnunev2@gmail.com>
2017-06-14 17:15:33 +0100

    add tableview delegate and datasource to the checklist controller 🙆🏼‍♂️

--- HolidayTime/EventChecklistViewController.swift
+++ HolidayTime/EventChecklistViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EventChecklistViewController: UIViewController {
+class EventChecklistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +18,19 @@ class EventChecklistViewController: UIViewController {
 
     @IBAction func donePressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Table View delegate:
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "")!
+        return cell
     }
 }
